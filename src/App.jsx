97d87a1fef7f95104a692ca4af8d878d51c6015b
DangDuyy <duyprovn987@gmail.com>
2025-06-08 22:19:08 +0700

    code layout cơ bản cho trello app web

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,58 @@
-import Button from '@mui/material/Button'
+import Container from '@mui/material/Container'
+import { useColorScheme } from '@mui/material/styles'
+import Box from '@mui/material/Box'
+function ModeSwitcher() {
+  const { mode, setMode } = useColorScheme()
+
+  if (!mode) return null
+
+  return (
+   <div>
+    Mode:
+     <select
+      value={mode}
+      onChange={e => setMode(e.target.value)}
+      style={{ marginBottom: 16 }}
+    >
+      <option value="system">System</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+   </div>
+  )
+}
+
 function App() {
   return (
-    <>
-    <div>duydep</div>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-    </>
+    <Container disableGutters maxWidth={false} sx={{ height: '100vh', backgroundColor:'primary.main'}}>
+      <Box sx={{
+        backgroundColor: 'primary.light',
+        width:'100%',
+        height: (theme) => theme.trello.appBarHeight, // Đổi sang custom
+        display: 'flex',
+        alignItems: 'center'
+      }}>
+        <ModeSwitcher/>
+      </Box>
+      <Box sx={{
+        backgroundColor: 'primary.dark',
+        width:'100%',
+        height: (theme) => theme.trello.boardBarHeight,
+        display: 'flex',
+        alignItems: 'center'
+      }}>
+        Board Bar
+      </Box>
+      <Box sx={{
+        backgroundColor: 'primary.main',
+        width:'100%',
+        height: (theme) => `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`,
+        display: 'flex',
+        alignItems: 'center'
+      }}>
+        Board Content
+      </Box>
+    </Container>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
+import CssBaseline from '@mui/material/CssBaseline'
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import CssBaseline from '@mui/material/CssBaseline'
+import theme from './theme.js' // Thêm dòng này
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CssBaseline />
-    <App />
+    <CssVarsProvider theme={theme}> {/* Truyền theme vào đây */}
+      <CssBaseline />
+      <App />
+    </CssVarsProvider>
   </React.StrictMode>,
 )
