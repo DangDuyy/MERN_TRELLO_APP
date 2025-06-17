a0710c67c99f79c1c92bcc5796a3dc223f5f061b
DangDuyy <duyprovn987@gmail.com>
2025-06-17 21:20:09 +0700

    ban đầu kéo thả các column bằng thư viện dnd-kit

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -1,19 +1,52 @@
+import { DndContext } from '@dnd-kit/core'
+import { arrayMove } from '@dnd-kit/sortable'
 import Box from '@mui/material/Box'
+import { useEffect, useState } from 'react'
+import { mapOrder } from '~/utils/sorts'
 import ListColumns from './ListColumns/ListColumns'
-import {mapOrder} from '~/utils/sorts'
 
 function BoardContent({board}) {
-  const orderedColumn = mapOrder(board?.columns, board?.columnOrderIds, '_id')
+  const [orderedColumnState, setOrderedColumnState] = useState([])
+
+  useEffect(() => {
+    setOrderedColumnState(mapOrder(board?.columns, board?.columnOrderIds, '_id'))
+  }, [board])
+
+  const handleDragEnd = (event) => {
+    console.log('event ', event )
+    const { active, over } = event
+
+    //kiểm tra nếu kéo thả linh tinh vùng ngoài thì return luôn tránh lỗi
+    if (!over) return
+
+    //nếu vị trí kéo thả khác với vị trí ban đầu
+    if (active.id !== over.id) {
+      //lấy vị trí cũ từ thằng áctive
+      const oldIndex = orderedColumnState.findIndex( c => c._id === active.id)
+      //lấy vị trí mới từ thằng over
+      const newIndex = orderedColumnState.findIndex( c => c._id === over.id)
+
+      const dndOrderedColumn = arrayMove(orderedColumnState, oldIndex, newIndex)
+      // const dndOrderedColumnIds = dndOrderedColumn.map( c => c._id)
+      // console.log('dndOrderedColumn', dndOrderedColumn)
+
+      //cập nhật statecolumn ban đàu sau khi kéo thả
+      setOrderedColumnState(dndOrderedColumn)
+    }
+  }
+
   return (
-    <Box sx={{
-      width:'100%',
-      height: (theme) => theme.trello.boardContentHeight,
-      // alignItems: 'center',
-      backgroundColor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2' ),
-      p: '10px 0'
-    }}>
-      <ListColumns columns={orderedColumn}/>
-    </Box>
+    <DndContext onDragEnd={handleDragEnd}>
+      <Box sx={{
+        width:'100%',
+        height: (theme) => theme.trello.boardContentHeight,
+        // alignItems: 'center',
+        backgroundColor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2' ),
+        p: '10px 0'
+      }}>
+        <ListColumns columns={orderedColumnState}/>
+      </Box>
+    </DndContext>
   )
 }
 
--- src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
@@ -13,24 +13,37 @@ import Menu from '@mui/material/Menu'
 import MenuItem from '@mui/material/MenuItem'
 import { useState } from 'react'
 import ListCard from './ListCards/ListCard'
-import {mapOrder} from '~/utils/sorts'
+import { mapOrder } from '~/utils/sorts'
+import {useSortable} from '@dnd-kit/sortable'
+import {CSS} from '@dnd-kit/utilities'
 
-function Column({column}) {
+function Column({ column }) {
+  //sortable context
+  const { attributes, listeners, setNodeRef, transform,transition} = useSortable({ id: column._id, data: {...column} })
+  const dndKitColumnStyles = {
+    transform: CSS.Translate.toString(transform),
+    transition
+  }
+
   const [anchorEl, setAnchorEl] = useState(null)
   const open = Boolean(anchorEl)
   const handleClick = (event) => setAnchorEl(event.currentTarget)
   const handleClose = () => setAnchorEl(null)
-  const orderedCard = mapOrder(column?.cards, column?.cardOrderIds,'_id')
+  const orderedCard = mapOrder(column?.cards, column?.cardOrderIds, '_id')
   return (
-    <Box sx={{
-      minWidth: 300,
-      maxWidth: 300,
-      backgroundColor: (theme) => (theme.palette.mode === 'dark' ? '#333643' : '#ebecf0' ),
-      ml: 2,
-      borderRadius: '6px',
-      height: 'fit-content',
-      maxHeight: (theme) => `calc(${theme.trello.boardContentHeight} - ${theme.spacing(5)})`
-    }}
+    <Box
+      ref={setNodeRef}
+      style={dndKitColumnStyles}
+      {...attributes} {...listeners}
+      sx={{
+        minWidth: 300,
+        maxWidth: 300,
+        backgroundColor: (theme) => (theme.palette.mode === 'dark' ? '#333643' : '#ebecf0' ),
+        ml: 2,
+        borderRadius: '6px',
+        height: 'fit-content',
+        maxHeight: (theme) => `calc(${theme.trello.boardContentHeight} - ${theme.spacing(5)})`
+      }}
     >
       {/* Box column header */}
       <Box sx={{
